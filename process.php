1a3c96ed7f88f76dea48c67dbbfe18e82f836222
Priti Vanzara <84765222+PritiVanzara@users.noreply.github.com>
2021-06-03 15:55:24 +0530

    Add files via upload

--- process.php
+++ process.php
@@ -1,11 +1,17 @@
 <?php
 
-//$_GET $_POST $_REQUEST // CAPITAL
-//['controlname']
-
 $a = $_POST['txt1'];
 $b = $_POST['txt2'];
+$c = $_POST['txt3'];
 
-echo "Hello My Name is $a and Age is $b";
-
-
+echo "<table border=1>";
+echo "<tr>
+    <th>Name</th>
+    <th>Age</th>
+    <th>Email</th>
+    </tr>
+    <tr>
+        <td>$a</td>
+        <td>$b</td>
+        <td>$c</td>
+    </tr>";
